af47af7d7223d28bcef34a1192394cc8377f8d5b
xuzhang0636 <363854939@qq.com>
2025-04-20 10:31:26 +0800

    feat: 🎸 init project
    init project

---
+++ src/main/java/com/xian/richard/RichardApplication.java
@@ -0,0 +1,13 @@
+package com.xian.richard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RichardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RichardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xian/richard/RichardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xian.richard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RichardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
